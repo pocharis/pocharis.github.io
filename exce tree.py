f32a6c01871a3a8862c592000edc3ea88e11fccf
honour2023 <oladeleibrahim75@gmail.com>
2017-09-08 18:01:53 +0000

    --all

---
+++ exce tree.py
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="description" content="Personal page">
+    <meta name="keywords" content="MEST,entrepreneurship,Incubation,goals,money,solution,technology">
+    <meta name="author" content="Ugwu Arinze Christopher">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--Font Awsome icon Link-->
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!--Google font link-->
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet"> 
+
+  </head>
+  <body class="contact_body">
+	    
+	    	<div id="menu-bar_contact">
+	    		<ul>
+	    			
+		    			<a href="index.html"><li>
+		    				<span>Welcome</span>
+		    			</li></a>
+
+	    				<a  href="about.html">
+		    			<li>
+		    				<span>Meet Me</span>
+		    			</li>
+	    				</a>
+
+	    				<!--<a  href="interests.html">
+		    			<li>
+		    				<span>Interests</span>
+		    			</li>
+	    				</a>-->
+
+	    				<a  href="contact.html">
+		    			<li>
+		    				<span>Reach Me</span>
+		    			</li>
+		    			</a>
+	    		
+	    		</ul>
+	 
+	    	<hr>
+		<section id="contact">
+			
+			<form>
+			<table>
+
+				<tr>
+					<td>
+						<label>Name:</label>
+					</td>
+
+					<td>
+						<input type="text" name="name">
+					</td>
+
+				</tr>
+
+				<tr>
+					<td>
+						<label>Email:</label>
+					</td>
+
+					<td>
+						<input type="text" name="name">
+					</td>
+
+				</tr>
+
+				<tr>
+					<td>
+						<label>Message:</label>
+					</td>
+
+					<td>
+						<textarea name="comment" form="usrform"  placeholder="Enter text here..." ></textarea>
+					</td>
+
+				</tr>
+
+				<tr>
+					<td>
+						
+					</td>
+
+					<td>
+						
+					<input id="submit" type="submit" name="submt" value="Submit">
+					</td>
+
+				</tr>
+
+			</table>
+			</form>
+
+
+
+			<div class="socials">
+
+			<table>
+				<tr>
+					<td>
+						<a href="instagram.com"><i class="fa fa-instagram fa-5x" aria-hidden="true"></i></a>
+					</td>
+
+					<td>
+						<a href="facebook.com"><i class="fa fa-facebook-square fa-5x" aria-hidden="true"></i></a>
+					</td>
+
+					<td>
+						<a href="twitter.com"><i class="fa fa-twitter fa-5x" aria-hidden="true"></i></a>
+					</td>
+
+					<td>
+						<a href="quora.com"><i class="fa fa-quora fa-5x" aria-hidden="true"></i></a></i></a>
+					</td>
+				</tr>
+
+			</table>
+				
+
+
+
+			</div>
+		</section>
+
+		</div>
+
+		
+</body>
+</html>
